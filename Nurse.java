1fb8226d0a2770b9f5e8ad7e56f6a1bcbdbcb162
WillCR11 <94143973+WillCR11@users.noreply.github.com>
2021-11-17 17:23:26 -0700

    Create Nurse.java

---
+++ Nurse.java
@@ -0,0 +1,46 @@
+//contains outline and data for all nurses, as of now is the same as doctor
+
+public class Nurse
+{
+	private String firstName;
+	private String lastName;
+	private String email;
+	private int employeeID;
+	private int phoneNum;
+	
+		//getters and setters for all patient information
+		
+		public String getFirstName() {
+			return firstName;
+		}
+		
+		public String getLastName() {
+			return lastName;
+		}
+		
+		public String getEmail() {
+			return email;
+		}
+		
+		
+		public int getPhoneNum() {
+			return phoneNum;
+		}
+		
+		public void setFirstName(String fn) {
+			 firstName = fn;
+		}
+		
+		public void setLastName(String ln) {
+			 lastName = ln;
+		}
+		
+		public void setEmail(String e) {
+			 email = e;
+		}
+		
+		
+		public void setPhoneNum(int pn) {
+			 phoneNum = pn;
+		}
+}
